7367c4c10d1a1cf7797a6fd31b02d942f08b672c
Lukasz Chmielewski <l.chmielewski@sportradar.com>
2019-10-03 09:03:15 +0200

    Fixed overflow

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -119,5 +119,6 @@ h1 {
   font-family: 'Nunito', 'Poppins', 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+  overflow: hidden;
 }
 </style>
